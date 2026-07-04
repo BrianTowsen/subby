80dd29da534d37bd3b60cd7dbdc8c3881063b8c8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-07-04 09:23:08 +0000

    Updating to latest FlutterFlow output.

--- lib/projects/add_projects_page/add_projects_page_widget.dart
+++ lib/projects/add_projects_page/add_projects_page_widget.dart
@@ -42,6 +42,7 @@ class _AddProjectsPageWidgetState extends State<AddProjectsPageWidget> {
       },
       child: Scaffold(
         key: scaffoldKey,
+        resizeToAvoidBottomInset: false,
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
         body: Container(
           width: double.infinity,
